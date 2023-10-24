a52e3d7dd4dccd706b52791af162f9f8af766432
GbyOtz <anagabrielaortizv16@gmail.com>
2023-10-24 11:00:32 -0400

    pagina 2 card

--- src/Pagina2.js
+++ src/Pagina2.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './componentes/card';
 
 const Pagina2 = () => {
   return (
     <div>
       <h1>Página 2</h1>
       {/* Contenido de la página */}
+      <Card />
     </div>
   );
 };
---
+++ src/componentes/card/index.js
@@ -0,0 +1,21 @@
+import './card.css'
+
+const Card=()=>{
+
+
+    return <div className='card'>
+        <div className='encabezado'>
+            <img src="" alt="imagen">
+
+            </img>
+        </div>
+        <div className='letras' >
+       
+            <h4>Direccion</h4>
+           
+         
+        </div>
+    </div>
+}
+
+export default Card;
